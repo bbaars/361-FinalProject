5420cc368cc694f2e8d49ad075605e6d7a337258
Brandon Baars <brandon@Brandons-MacBook-Pro.local>
2017-06-05 16:21:09 -0400

    Validation Changed and added Comments

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
  * @Date:   Saturday, June 3rd 2017, 2:04:24 pm
  * @Filename: main.c
  * @Last modified by:   brandon
- * @Last modified time: Sunday, June 4th 2017, 10:40:44 pm
+ * @Last modified time: Monday, June 5th 2017, 4:19:49 pm
  *
  * CIS 361 Final Project
  * GREP Simulator using c in a UNIX Environment
@@ -18,9 +18,10 @@
 #include <string.h>
 #include <time.h>
 
+void printUsage();
+_Bool checkValidInput(char * option);
 
 int main(int argc, char const *argv[]) {
-  /* code */
 
   char *logname = getenv("LOGNAME");
   char filename[256], line[1024], terminal[100], cwd[512], command[7], option[10], parameter[512];
@@ -35,8 +36,9 @@ int main(int argc, char const *argv[]) {
   strcpy(command, "");
 
 /* simulates command line while the user doesn't enter ':x' to terminate */
-  while(strcmp(command, terminateCmd) != 0) {
+  while(1) {
 
+    /* simulates a terminal by displaying the device and logname */
     printf("Brandons-MacBook-Pro:~ %s$ ", logname);
     fgets(terminal, 100, stdin);
 
@@ -47,32 +49,81 @@ int main(int argc, char const *argv[]) {
     if (!strcmp(command, "pwd"))
         printf("%s\n", getcwd(cwd, 512));
 
-    /* if input command is not grep */
-    else if (strcmp(command, "grep") != 0) // >0 or <0 if false,  ==0 if true
-      printf("Sorry '%s' not recognized \n", command);
 
-      /*
-      * TODO: MAKE SURE WHEN USER ENTERS OPTIONS, THERE ARE THE  *  REST OF THE PARAMETERS
-      */
+      // TODO: IF ALL INPUT IS VALID, call corresponding functions
 
-    /* if the [-options] have been left out */
-    else if (strcmp(option, "-i") != 0) {
-      printf("Sorry, options not recognized\n");
+    /* if the command is grep */
+    else if (!strcmp(command, "grep")) {
 
-      char temp[512];
-      strcpy(temp, parameter);
-      strcpy(parameter, option);
-      strcpy(filename, temp);
+      /* checks for a valid command */
+      if(checkValidInput(option)) {
+
+          /* checks for next inputs to contain items */
+          if(strcmp(parameter, "") != 0 && strcmp(filename, "") != 0) {
+            printf("VALID GREP\n");
+          } else {
+            printUsage();
+          }
+      /* if no commands, checks that it contains items */
+      } else {
+          /* if a command was left out and insteat the parameters were left in, then the command variable now becomes the parameters and the file name takes the place of parameters, ONLY if the filename entered is empty, if there are 4 inputs and no valid command then it is an invalid input
+
+          grep [-options] [parameters] [filename]
+
+          example: grep file file file // invalid, too many arguements
+                  grep parameter filename
+          */
+          if(strcmp(parameter, "") != 0 && strcmp(filename, "") == 0) {
+            printf("VALID GREP\n");
+            char temp[512];
+            strcpy(temp, parameter);
+            strcpy(parameter, option);
+            strcpy(filename, temp);
+          } else {
+            printUsage();
+          }
+      }
+
+    /* exit command of program */
+    } else if (!strcmp(command, ":x")) {
+        return 0;
     }
 
-    printf("%s\n", parameter);
-    printf("%s\n", filename);
+    else {
+      printf("Sorry, '%s' not recognized\n", command);
+    }
 
+    /* flushing input buffer */
     fflush(stdin);
+
+    /* resets variables to empty after each cycle this way they don't get copied over the next time around */
+    strcpy(option, "");
+    strcpy(parameter, "");
+    strcpy(filename, "");
 
   }
 
     return 0;
+}
+
+/*
+* Prints the grep command and the options with their descriptions to   * the user whenever invalid input has been entered.
+*/
+void printUsage() {
+  puts("usage: grep [-options] [contentToSearchFor] [fileToSearchIn]");
+  puts("[-c] Print only count of the lines\n[-i] iGnOre CaSe\n[-l] List only filenames of files containing a match\n[-n] Gives line number of pattern match\n[-v] Reverses search giving NOT containing string\n[-w] Only contain whole word matches\n[-x] Only contain whole line matches.");
+}
+
+/*
+* Checks the input commands from the user and return true if it's       * valid, or false if it's invalid. 
+*/
+_Bool checkValidInput(char * option) {
+
+  if(!strcmp(option, "-i") || !strcmp(option, "-c") || !strcmp(option, "-l") || !strcmp(option, "-n") || !strcmp(option, "-v") || !strcmp(option, "-x") || !strcmp(option, "-w")) {
+    return 1;
+  }
+
+  return 0;
 }
 
   // while((currententry=readdir(d))!=NULL){
